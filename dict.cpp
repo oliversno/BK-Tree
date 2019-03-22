0653e2a4ac78cd97c7db6665f3fb3f47d6504cc8
Sean Oliver <snoliver@umich.edu>
2019-03-21 23:06:37 -0400

    implimented add and contains for Dictionary

--- dict.cpp
+++ dict.cpp
@@ -1,1 +1,11 @@
 #include "dict.h"
+
+void Dictionary::add(const std::string& new_word) {
+        if(!dict.insert(new_word).second){
+        throw InsertFailed();
+    }
+}
+
+bool Dictionary::contains(const std::string& query) const {
+    return dict.find(query) != dict.end();
+}
--- dict.h
+++ dict.h
@@ -1,4 +1,20 @@
 #ifndef DICT_H
 #define DICT_H
 
+#include <unordered_set>
+#include <string>
+
+class Dictionary{
+    private:
+    std::unordered_set<std::string> dict;
+
+    public:
+    // adds new_word to the dictionary
+    // throws a InsertFailed Exception if the value can not be added
+    void add(const std::string& new_word);
+
+    // returns true if query is in thhe dictionary, false otherwise
+    bool contains(const std::string& query) const;
+};
+
 #endif
