fce2b58d3cc30ff15aab27e302184ca1dea5c388
Sean Oliver <snoliver@umich.edu>
2019-03-22 20:16:46 -0400

    outline for bktree

--- bktree.h
+++ bktree.h
@@ -1,4 +1,31 @@
 #ifndef BKTREE_H
 #define BKTREE_H
 
+#include<string>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+
+class Node{
+    private:
+    std::string word;
+    std::unordered_map<int, Node*> children;
+
+    public:
+    //ctor
+    Node(std::string word_);
+
+    //dtor
+    ~Node();
+
+    void addChild(int key, std::string& word);
+
+    std::vector<int> keys() const;
+
+    bool containsKey(int key) const;
+
+    Node &operator[](int i);
+
+};
+
 #endif
